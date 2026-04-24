bfc6caa750ed1f97c8e4b3e90fab05f2f7f9e146
Pranav <oruganti.pranav.datta@gmail.com>
2026-04-24 12:56:58 +0530

    refactor: dispatch table for specifiers

--- fart.c
+++ fart.c
@@ -3,35 +3,49 @@
 #include <stdarg.h>
 #include <string.h>
 
-static void put_str(const char *s)
-{
-    write(1, s, strlen(s));
-}
-static void put_char(char c)
-{
-    write(1, &c, 1);
-}
+static void put_char(char c)    { write(1, &c, 1); }
+static void put_str(const char *s) { write(1, s, strlen(s)); }
 
 static void put_int(int n)
 {
     char buf[12];
     int i = 0;
-
     if (n < 0) { write(1, "-", 1); n = -n; }
     if (n == 0) { write(1, "0", 1); return; }
     while (n > 0) { buf[i++] = '0' + (n % 10); n /= 10; }
     while (i--) write(1, &buf[i], 1);
 }
+
 static void put_hex(unsigned int n)
 {
     char *digits = "0123456789abcdef";
     char buf[8];
     int i = 0;
-
     if (n == 0) { write(1, "0", 1); return; }
     while (n > 0) { buf[i++] = digits[n % 16]; n /= 16; }
     while (i--) write(1, &buf[i], 1);
 }
+
+typedef struct {
+    char    spec;
+    void    (*fn)(va_list *);
+} t_handler;
+
+static void handle_s(va_list *args) { put_str(va_arg(*args, char *)); }
+static void handle_c(va_list *args) { put_char(va_arg(*args, int)); }
+static void handle_d(va_list *args) { put_int(va_arg(*args, int)); }
+static void handle_x(va_list *args) { put_hex(va_arg(*args, unsigned int)); }
+static void handle_pct(va_list *args) { (void)args; put_char('%'); }
+
+static t_handler handlers[] = {
+    {'s', handle_s},
+    {'c', handle_c},
+    {'d', handle_d},
+    {'x', handle_x},
+    {'%', handle_pct},
+    {0, NULL}
+};
+
 int fart(const char *fmt, ...)
 {
     va_list args;
@@ -40,28 +54,20 @@ int fart(const char *fmt, ...)
     int i = 0;
     while (fmt[i])
     {
-        if (fmt[i] == 's')
-            put_str(va_arg(args, char *));
-        else if (fmt[i] == 'c')
-            put_char(va_arg(args, int));
-        else if (fmt[i] == 'd')
-            put_int(va_arg(args, int));
         if (fmt[i] == '%' && fmt[i + 1])
         {
             i++;
-            if (fmt[i] == 's')
-                put_str(va_arg(args, char *));
-        }
-        else if (fmt[i] == 'x'){
-            put_hex(va_arg(args, unsigned int));
-        }
-        else if (fmt[i] == '%'){
-            put_char('%');
+            int j = 0;
+            while (handlers[j].spec)
+            {
+                if (handlers[j].spec == fmt[i])
+                    { handlers[j].fn(&args); break; }
+                j++;
+            }
         }
         else
             write(1, &fmt[i], 1);
         i++;
-        
     }
     va_end(args);
     return 0;
