6f7cab23cfe427811059abdaa40686e54f8dff3f
Pranav <oruganti.pranav.datta@gmail.com>
2026-04-24 12:58:00 +0530

    feat: return char count + final tests

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include "fart.h"
+#include <stdio.h>
 
 int main(void)
 {
-    fart("hex: %x, literal: 100%%\n", 255);
+    int f_ret = fart("hello %s, number %d, hex %x\n", "world", 42, 255);
+    int p_ret = printf("hello %s, number %d, hex %x\n", "world", 42, 255);
+    fart("fart returned: %d\n", f_ret);
+    fart("printf returned: %d\n", p_ret);
     return 0;
 }
